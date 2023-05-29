ed3bda7d73b85f219d35e23f411537dd52bf2874
isu-nice <isu010614@naver.com>
2023-05-29 14:24:39 +0900

    feat: concat() 기본 예제 2

---
+++ src/main/java/com/states/webflux/operators/transformation/ConcatExample02.java
@@ -0,0 +1,20 @@
+package com.states.webflux.operators.transformation;
+
+import com.states.webflux.operators.sample_data.SampleData;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+// concat() 기본 예제 2
+@Slf4j
+public class ConcatExample02 {
+    public static void main(String[] args) {
+        Flux.concat(
+                        Flux.fromIterable(SampleData.salesOfCafeC),
+                        Flux.fromIterable(SampleData.salesOfCafeB),
+                        Flux.fromIterable(SampleData.salesOfCafeC)
+                ).reduce((a, b) -> a + b)
+                .subscribe(data -> log.info("# total sales: {}", data));
+    }
+
+    // concat() 으로 월별 매출액을 모두 하나의 sequence로 연결한 다음 카페의 전체 매출액의 계산
+}
